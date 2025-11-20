339410ced2022d1459696ef8b2a36db813d8facf
mrethion25 <worldof2512@gmail.com>
2025-11-20 19:35:03 +0530

    Enhance static file serving configuration
    Updated static file serving to include extensions and set index file.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname));
+// Ye line add kar - sab files serve karne ke liye
+app.use(express.static(__dirname, {
+  extensions: ['html', 'css', 'js'],
+  index: 'index.html'
+}));
 
-// Handle all routes
+// Sab routes ko handle kar
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = app;
